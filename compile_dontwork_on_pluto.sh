4b364bf2eebbd802b06344137fe538edb1d687b5
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2014-12-29 09:52:12 +0100

    Update compile script for non-pluto hosts

--- compile_dontwork_on_pluto.sh
+++ compile_dontwork_on_pluto.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# Note:
+# - you need to apt-get install gtk-doc-tools 
+# - you need to download gstreamer, gst-plugins-base, gst-plugins-bad and build them from source in that order (./autogen.sh && make)
+# - libgtk-3-dev # for gtk/gtk.h, not really needed, I guess...
+# - you need to download gst-plugins-good and build it from source to have avidemux (for video/msvideo) 
+# - you need to download gst-libav and build it from source to have more audio and video decoders
+
+
 prefix=/usr/lib/x86_64-linux-gnu/
 
 
@@ -28,10 +37,10 @@ ARGS2=$($SDL_CONFIG --cflags);
 if uname -s | grep -i -c "LINUX" > /dev/null; then 
     echo "GNU/Linux detected.";
     echo "compiling...";    
-    #gcc $ARGS1 $ARGS2 -I/usr/local/include -I/usr/include/ -I/usr/X11R6/include -L/usr/lib/ -L/usr/local/lib/ -L/usr/X11R6/lib -lSDL_image -lGL -lGLU -lglut -lXmu -lXi -lXext -lX11 -lm -lsmpeg -D_THREAD_SAFE -O2 -g -x c $(pkg-config --libs --cflags gstreamer-0.10) -o tdfsb tdfsb.c;
-	#options="/usr/lib/x86_64-linux-gnu/libX11.so /usr/lib/x86_64-linux-gnu/libSDL.so $prefix/libgstreamer-0.10.so $prefix/libgobject-2.0.so $prefix/libgstinterfaces-0.10.so.0.25.0"
 	options="/usr/lib/x86_64-linux-gnu/libX11.so -Wl,$prefix/libsmpeg-0.4.so.0.1.4"
-    gcc -v $ARGS1 $ARGS2 -I/usr/local/include -I/usr/include/ -I/usr/X11R6/include -L/usr/lib/ -L/usr/local/lib/ -L/usr/X11R6/lib -D_THREAD_SAFE -O2 -g -x c $(pkg-config --libs --cflags gstreamer-0.10) -lSDL_image -lGL -lGLU -lglut -lXmu -lXi -lXext -lX11 -lm -lsmpeg -o tdfsb \
+	gtkstuff="-I /usr/include/gtk-3.0/ -I /usr/include/cairo -I /usr/include/pango-1.0 -I /usr/include/gdk-pixbuf-2.0 -I /usr/include/atk-1.0"
+	includes="-I/usr/local/include/gstreamer-1.0 $gtkstuff"
+    gcc -v $ARGS1 $ARGS2 -I/usr/local/include -I/usr/include/ -I/usr/X11R6/include $includes -L/usr/lib/ -L/usr/local/lib/ -L/usr/X11R6/lib -D_THREAD_SAFE -O2 -g -x c $(pkg-config --libs --cflags gstreamer-1.0) $(pkg-config --libs --cflags gstreamer-gl-1.0) $(pkg-config --libs --cflags gstreamer-video-1.0) $(pkg-config --libs --cflags gdk-pixbuf-2.0) -lSDL_image -lGL -lGLU -lglut -lXmu -lXi -lXext -lX11 -lm -lsmpeg -o tdfsb \
 -Wl,$options \
 tdfsb.c;
 elif uname -s | grep -i -c "BEOS" > /dev/null; then 
