3f811ba1ea71cf394608cdc92d47643c8826ba5a
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2015-01-06 12:59:38 +0100

    Improve comments

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+# Quick and dirty script that I use to run 3dfsb,
+# because I've compiled gstreamer-1.xx from source and installed it in /usr/local/lib/
 LD_LIBRARY_PATH=/usr/local/lib/gstreamer-1.0  ./3dfsb
