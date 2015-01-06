66466b1074110853c3b15eaf68cce9c884cabc36
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2015-01-06 13:00:04 +0100

    Improve comments

--- debug.sh
+++ debug.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+# Quick and dirty script that I use to debug 3dfsb,
+# because I've compiled gstreamer-1.xx from source and installed it in /usr/local/lib/
 export LD_LIBRARY_PATH=/usr/local/lib/gstreamer-1.0 ; gdb ./3dfsb
