dcfe9c2640959121efc9294e253dea5c2c29c993
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2015-01-17 16:19:30 +0100

    indent -linux -l1200 -lc1200 str_replace.*

--- str_replace.h
+++ str_replace.h
@@ -12,7 +12,6 @@
 // str_replace() returns haystack on success and NULL on failure. 
 // Failure means there was not enough room to replace all occurences of oldneedle.
 // Success is returned otherwise, even if no replacement is made.
-char *str_replace(char *haystack, size_t haystacksize,
-                    const char *oldneedle, const char *newneedle);
+char *str_replace(char *haystack, size_t haystacksize, const char *oldneedle, const char *newneedle);
 
 #endif
