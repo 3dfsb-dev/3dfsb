01bfae1ed08a31c4e46e90664fc69c79de8d5f20
Tom Van Braeckel <tomvanbraeckel@gmail.com>
2015-02-01 18:01:41 +0100

    Improve comments

--- tools.c
+++ tools.c
@@ -4,15 +4,13 @@
 
 #include "tools.h"
 
-// xdg-mime query default $(file --mime-type -b filename)
 char *execute_binary(void) {
 
 	FILE *fp;
 	char path[1035] = { 0 };
 	char *toreturn = malloc(sizeof(char) * 10240);
 
-	/* Open the command for reading. */
-	//fp = popen("/bin/ls /etc/", "r");
+	// xdg-mime query default $(file --mime-type -b filename)
 	fp = popen("/usr/bin/xdg-mime query default text/plain", "r");
 	if (fp == NULL) {
 		printf("ERROR: Failed to run the command\n" );
